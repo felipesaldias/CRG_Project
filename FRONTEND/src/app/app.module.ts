65c67c116e3640990ec662759cba6ce6a6467101
Felipe <felipe.saldias@alumnos.uach.cl>
2019-09-29 15:58:22 -0600

    enrutado funcionando
    se logro hacer funcionar el routing

--- FRONTEND/src/app/app.module.ts
+++ FRONTEND/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routing, appRoutingProviders} from './components/app.routing';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class UiModule { }
+
