512f75e2d1deea603042431cbe882f3582ae6ac9
Nicolas <nikolasotarola@alumnos.uach.cl>
2020-02-18 22:16:53 -0300

    add login component and form and its front-end validation2

--- FRONTEND/web_crg/src/App.js
+++ FRONTEND/web_crg/src/App.js
@@ -2,11 +2,16 @@ import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AddTrainer from './components/AddTrainer';
+import Login from './components/Login';
 
 function App() {
   return (
-    <AddTrainer>
-    </AddTrainer>
+    <Fragment>
+      <AddTrainer>
+      </AddTrainer>
+    </Fragment>
+    
+
     
   );
 }
