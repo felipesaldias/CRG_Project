6767aaf54bbc7c42ef6e1fdaeed6c9026e804b25
fsaldias <felipesaldias@gmail.com>
2020-02-23 23:20:45 -0300

    query and url params immplemented

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,10 +2,21 @@ var express = require('express')
 var http = require('http')
 var app = express()
 
-app.get('/', (req, res) => {
+app.get('/:id', (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.status(200).send("Welcome to API REST")
+  //res.status(200).send("Welcome to API REST")
+  console.log(req.params)
+  console.log(req.query)
+  var junto2= {"params": req.params, "query": req.query}
+  res.status(200).send(junto2)
+  
+})
+
+app.get('/dani', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.status(200).send("hola soy la dani")
 })
 
 http.createServer(app).listen(8001, () => {
