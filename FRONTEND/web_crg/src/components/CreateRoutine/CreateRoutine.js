cf9127e3c57240fd7cd94b027d8192556ff86e5a
NicolasOtarolaTeillier <nicolas.otarola@alumnos.uach.cl>
2020-03-22 02:17:50 -0300

    profile links create

---
+++ FRONTEND/web_crg/src/components/CreateRoutine/CreateRoutine.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CreateRoutine() {
+    return (
+        <div>
+            CreateRoutine
+        </div>
+    )
+}
--- FRONTEND/web_crg/src/pages/CrgPanel/CrgPanel.js
+++ FRONTEND/web_crg/src/pages/CrgPanel/CrgPanel.js
@@ -6,6 +6,10 @@ import UserList from '../../components/UsersList/UserList';
 import Profile from '../../components/Profile/Profile';
 import CreateAccount from '../../components/CreateAccount';
 import EditUser from '../../components/EditUser/EditUser';
+import CreateRoutine from '../../components/CreateRoutine/CreateRoutine';
+import LoadPdf from '../../components/LoadPdf/LoadPdf';
+import Calendar from '../../components/Calendar/Calendar';
+
 import { Link,BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 
@@ -30,7 +34,11 @@ const CrgPanel = props => {
         <ul>
             <li><Link to='/crg/panel/newaccount'>Crear Usuario</Link></li>
             <li><Link to='/crg/panel/user'>Listado de Usuarios</Link></li>
-            <li><Link to='/crg/panel/user/edit'>Editar un usuario</Link></li>
+            <li><Link to='/crg/panel/user/edit'>Editar un Usuario</Link></li>
+            <li><Link to='/crg/panel/user/createroutine'>Crear una Rutina</Link></li>
+            <li><Link to='/crg/panel/user/loadpdf'>Subir Pdf</Link></li>
+            <li><Link to='/crg/panel/user/calendar'>Mostrar Calendario</Link></li>
+
           </ul>
         </div>
         <div>
@@ -38,12 +46,16 @@ const CrgPanel = props => {
             <Route exact path ="/crg/panel/profile" component={Profile}/>
             <Route exact path ="/crg/panel/newaccount" component={CreateAccount} /> 
             <Route exact path ="/crg/panel/user" component={UserList} />
-            <Route exact path ="/crg/panel/user/edit" component={EditUser} />  
+            <Route exact path ="/crg/panel/user/edit" component={EditUser} /> 
+            <Route exact path ="/crg/panel/user/createroutine" component={CreateRoutine}/>
+            <Route exact path ="/crg/panel/user/loadpdf" component={LoadPdf}/>
+            <Route exact path ="/crg/panel/user/calendar" component={Calendar}/>
           </Switch>
         </div>
       </div>       
     );
   }
-
+  
+  //<Route exact path ="/crg/panel/user/routine" component={}/> 
 
 export default CrgPanel;
