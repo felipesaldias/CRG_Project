59e9891338ff62ff47d92e124f6b75fc2cfbfd18
fsaldias <felipesaldias@gmail.com>
2020-04-04 14:24:33 -0300

    login styles

--- FRONTEND/web_crg/src/pages/Login/Login.js
+++ FRONTEND/web_crg/src/pages/Login/Login.js
@@ -45,36 +45,68 @@ const Login = (props) =>  {
       }
     })
     return (
-      
-      <div>
-        <form className= "form">
-          <input
-          type="text"
-          name="rut"
-          className="ocupa_mitad"
-          placeholder="Ingrese rut"
-          onChange={handleState}
-          value={rut}
-    
-          />
-          <input
-          type="password"
-          name="password"
-          className="ocupa_mitad"//"u-full-width"
-          placeholder="Ingrese password"
-          onChange={handleState}
-          value={password}
-          />
-          <button
-            type="submit"
-            name="submit"
-            className="ocupa_mitad button-primary"
-            onClick={submit}
-            >
-            Login
-          </button>
-        </form>
+     
+      <div class="cotainer">
+          <div class="row justify-content-center align-items-center vh-100">
+              <div class="col-md-5">
+                  <div class="card">
+                      <div class="card-header">Login</div>
+                        <div class="card-body">
+                          <form>
+                              <div class="form-group row">
+                                  <label for="email_address" class="col-md-4 col-form-label text-md-right text-dark">Usuario</label>
+                                  <div class="col-md-6">
+                                  <input
+                                    class="form-control"
+                                    type="text"
+                                    name="rut"
+                                    //className="ocupa_mitad"
+                                    placeholder="Ingrese rut"
+                                    onChange={handleState}
+                                    value={rut}
+                                    />
+                                  </div>
+                              </div>
+  
+                              <div class="form-group row">
+                                  <label for="password" class="col-md-4 col-form-label text-md-right text-dark">Contraseña</label>
+                                  <div class="col-md-6">
+                                  <input
+                                    class="form-control"
+                                    type="password"
+                                    name="password"
+                                    //className="ocupa_mitad"//"u-full-width"
+                                    placeholder="Ingrese password"
+                                    onChange={handleState}
+                                    value={password}
+                                    />
+                                  </div>
+                              </div>
+  
+  
+                              <div class="col-md-6 offset-md-4">
+                              <button
+                                type="submit"
+                                name="submit"
+                                class="btn btn-info"
+                                //className="ocupa_mitad button-primary"
+                                onClick={submit}
+                                >
+                                Login
+                              </button>
+                            </div>
+                        
+                      </form>
+                  </div>
+              </div>
+          </div>
       </div>
+    </div>
+      
+  
+  
+  
+    
     );
   
 }
